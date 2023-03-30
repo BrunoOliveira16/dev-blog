20807d9a60b45549edfb196b416960c1eed7905c
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-03-30 20:14:16 -0300

    Feat: Criado hook autenticação e configurado auth

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,29 +1,96 @@
 import './register.sass';
+
 import { useState, useEffect } from 'react';
+import { useAuthentication } from '../../hooks/useAuthentication';
 
 const Register = () => {
+  const [ name, setName ] = useState("")
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+  const [ confirmPassword, setConfirmPassword] = useState("")
+  const [ error, setError ] = useState("")
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      name,
+      email,
+      password
+    };
+
+    if(password !== confirmPassword) {
+      setError("As senhas precisam ser iguais!")
+      return;
+    }
+
+    const response = await createUser(user);
+
+    console.log(user)
+  };
+
+  useEffect(() => {
+    if(authError) {
+      setError(authError);
+    }
+  }, [authError])
+
   return (
-    <div>
+    <div className="register">
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe suas postagens!</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Nome:</span>
-          <input type="text" name='displayName' required placeholder='Nome do Usuário'/>
+          <input 
+            type="text" 
+            name="displayName" 
+            required 
+            placeholder="Nome do Usuário"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <label>
           <span>E-mail:</span>
-          <input type="email" name='displayEmail' required placeholder='E-mail do Usuário'/>
+          <input 
+            type="email" 
+            name="displayEmail" 
+            required 
+            placeholder="E-mail do Usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           <span>Senha:</span>
-          <input type="password" name='password' required placeholder='Insira sua senha'/>
+          <input 
+            type="password" 
+            name="password" 
+            required 
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <label>
           <span>Confirmação de Senha:</span>
-          <input type="password" name='confirmPassword' required placeholder='Confirme a sua senha'/>
+          <input 
+            type="password" 
+            name="confirmPassword" 
+            required 
+            placeholder="Confirme a sua senha"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </label>
-        <button className='btn'>Cadastrar</button>
+        {!loading &&  <button className="btn">Cadastrar</button>}
+        {loading &&  <button className="btn" disabled>Aguarde...</button>}
+        {error && <p className="error">{error}</p>}
       </form>
     </div>
   );
