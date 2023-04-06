7ff914725c8dd012fea20c8700790ec87ee406a8
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-04-06 12:21:12 -0300

    Feat: Criando estrutura e sass da pagina Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,40 @@
 import './Home.sass';
 
+// Hooks
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+// Components
+
 const Home = () => {
+  const [querry, setQuerry] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="home">
+        <h1>Veja aqui os posts mais recentes</h1>
+        <form onSubmit={handleSubmit} className="search_form">
+          <input 
+            type="text" 
+            placeholder="Ou busque por tags..." 
+            onChange={(e) => setQuerry(e.targe.value)} 
+            value={querry}
+          />
+          <button className='btn btn-dark'>Pesquisar</button>
+        </form>
+        <div>
+          <h1>Posts...</h1>
+          {posts && posts.length === 0 && (
+            <div className="noposts">
+              <p>Não Foram encontrados posts</p>
+              <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+            </div>
+          )}
+        </div>
     </div>
   );
 };
